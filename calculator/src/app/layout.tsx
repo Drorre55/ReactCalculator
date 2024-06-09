9f7f615fa4d0ca8c342f4dc8dadbadb08af88552
Drorre55 <dror604604@gmail.com>
2024-06-09 15:16:36 +0300

    [Add] toggle theme mode

--- calculator/src/app/layout.tsx
+++ calculator/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme/ThemeProvider"
+import { ThemeModeToggle } from "@/components/theme/ThemeModeToggle";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <ThemeModeToggle></ThemeModeToggle>
+                <body className={inter.className}>{children}</body>
+            </ThemeProvider>
+        </body>
     </html>
   );
 }
